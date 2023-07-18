bfe4c93bcb2b3a39816134f653ab7861639e1838
shivamtrip-st <shivamtripqthist@gmail.com>
2023-07-18 15:46:02 +0530

    This is crypto tracker app

---
+++ src/components/Exchanges.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {  Row, Col } from 'antd';
+import { useGetExchangesQuery } from '../services/cryptoApi';
+import Loader from './Loader';
+
+const Exchanges = () => {
+  const { isFetching } = useGetExchangesQuery();
+ // Note: To access this endpoint you need premium plan
+  if (isFetching) return <Loader />;
+  return (
+    <>
+    <Row>
+        <Col span={6}>Exchanges</Col>
+        <Col span={6}>24h Trade Volume</Col>
+        <Col span={6}>Markets</Col>
+        <Col span={6}>Change</Col>
+      </Row>
+      <Row>
+      </Row>
+    </>
+  );
+};
+
+export default Exchanges;
